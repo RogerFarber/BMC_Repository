cf1bdaf5f1fc884bd7e58ad92c59562e970e64b0
Roger.Farber <rogerfarber@gmail.com>
2021-06-22 11:02:55 +0300

    Edited Code

--- my-app/src/main/java/com/mycompany/app/App.java
+++ my-app/src/main/java/com/mycompany/app/App.java
@@ -4,10 +4,15 @@ package com.mycompany.app;
  * Hello world!
  *
  */
+
 public class App 
 {
+    public String SomeString() {
+        return "Hello World!";
+    }
     public static void main( String[] args )
     {
-        System.out.println( "Hello World!" );
+        App myApp = new App();
+        System.out.println(myApp.SomeString());
     }
 }
